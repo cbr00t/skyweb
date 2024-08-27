fb8e2f50a1cce426c86a3a0dbb65d21f35086e4e
Özer <83964758+cbr00t@users.noreply.github.com>
2024-08-27 16:08:49 +0300

    Tablet: Bozuk dosya düzeltildi

--- _kernel/config.php
+++ _kernel/config.php
@@ -1,1 +1,1 @@
-<?php $siteVersion = '2.64.28'?>
+<?php $siteVersion = '2.64.29'?>
--- app/prog/cetApp/part/cetBekleyenUgramaFisGirisPart.js
+++ app/prog/cetApp/part/cetBekleyenUgramaFisGirisPart.js
@@ -1,10 +1,6 @@
 (function() {
 	window.CETBekleyenUgramaFisGirisPart = class extends window.CETBekleyenXFisGirisPart {
-		constructor(e) {
-			e = e || {};
-			super(e);
-		}
-
 		static get partName() { return 'cetBekleyenUgramaFisGiris' }
+		constructor(e) { e = e || {}; super(e) }
 	}
 })()
